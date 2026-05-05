02da6ca36c80c6b2fdab95c05faf7647d4a481d5
Simona Miceva <simona.miceva18@gmail.com>
2026-05-05 21:46:37 +0200

    feat: improve login UI design

---
+++ frontend/src/data/features.js
@@ -0,0 +1,26 @@
+import logo from "../assets/images/logo.svg";
+import trending from "../assets/images/trending-up.svg"
+import info from "../assets/images/info-triangle1.svg"
+import users from "../assets/images/users.svg"
+export const data = [
+  {
+    title: "1,247",
+    desc: "Регистрирани дрвја",
+    img: logo,
+  },
+  {
+    title: "23",
+    desc: "Висок ризик",
+    img: info,
+  },
+  {
+    title: "94%",
+    desc: "Точност на предвидување",
+    img: trending,
+  },
+  {
+    title: "342",
+    desc: "Активни корисници",
+    img: users,
+  },
+];
